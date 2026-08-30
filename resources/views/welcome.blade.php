adc0cc009c518731edc295140f54e41e02377c6f
Deft Valian Exanova <deftvalian2411@gmail.com>
2026-08-30 21:40:27 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -420,8 +420,10 @@
                                 </div>
 
                                 <div class="flex items-center gap-4 border-t border-pink-100 dark:border-slate-800 pt-6">
-                                    <div class="w-12 h-12 rounded-full overflow-hidden border-2 border-white dark:border-slate-700 shadow-md flex-shrink-0">
-                                        <img src="https://i.pravatar.cc/150?u={{ $t->id }}" alt="Avatar" class="w-full h-full object-cover">
+                                    <div class="w-12 h-12 rounded-full bg-gradient-to-br from-pink-400 to-purple-500 text-white flex items-center justify-center border-2 border-white dark:border-slate-700 shadow-md flex-shrink-0">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.6-7.812-1.7a.75.75 0 01-.437-.695z" clip-rule="evenodd" />
+                                        </svg>
                                     </div>
                                     <div>
                                         <h4 class="font-black text-slate-800 dark:text-white text-lg">{{ $t->nama }}</h4>
